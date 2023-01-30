f9e899b183e6ce843d60f30d4e0c0c46dad22892
xiemin2161 <986778241@qq.com>
2023-01-30 14:17:53 +0800

    '提交'

---
+++ .history/Hello_20230130101005.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="item in items" :key="item.id">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: value,
+    };
+  },
+  methods: {
+    name() {},
+  },
+  computed: {
+    name() {
+      return this.data;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
